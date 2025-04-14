ef80f0271725ccd95ce9626950cdeb3664522cd3
Oyin1-2 <oyewoleakeem.tunde@gmail.com>
2025-04-14 02:12:50 +0100

    Second is charm

--- src/App.jsx
+++ src/App.jsx
@@ -206,7 +206,7 @@ export default function Navbar() {
 
           <div className="w-full max-w-xs">
             <a
-              href="https://multi-step-form-a2plgl0fz-oyin1-2s-projects.vercel.app"
+              href="https://multi-step-form-a2plgl0fz-oyin1-2s-projects.vercel.app/info"
               target="_blank"
             >
               <img
